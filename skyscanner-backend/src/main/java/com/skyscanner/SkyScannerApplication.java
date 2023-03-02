0c3ac74b5cacd1a6c34b1d16c355f3498637f920
Elvis Espinoza <elvis.espinoza.navarrete@outlook.com>
2023-03-02 13:50:20 -0500

    wip: read and combine search results from JSON files

--- skyscanner-backend/src/main/java/com/skyscanner/SkyScannerApplication.java
+++ skyscanner-backend/src/main/java/com/skyscanner/SkyScannerApplication.java
@@ -1,8 +1,14 @@
 package com.skyscanner;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SkyScannerApplication extends Application<SkyScannerConfiguration> {
 
@@ -21,7 +27,28 @@ public class SkyScannerApplication extends Application<SkyScannerConfiguration> 
     }
 
     @Override
-    public void run(final SkyScannerConfiguration configuration, final Environment environment) {
+    public void run(final SkyScannerConfiguration configuration, final Environment environment) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        // Read the json file from the resources folder
+        // and convert its JSON data to an array of SearchResult objects.
+        List<SearchResult> carResults = Arrays.asList(
+                mapper.readValue(
+                        getClass().getClassLoader().getResource("rental_cars.json"),
+                        SearchResult[].class
+                )
+        );
+
+        List<SearchResult> hotelResults = Arrays.asList(
+                mapper.readValue(
+                        getClass().getClassLoader().getResource("hotels.json"),
+                        SearchResult[].class
+                )
+        );
+        List<SearchResult> searchResults = new ArrayList<>();
+
+        // Add all the SearchResult objects from json files
+        searchResults.addAll(carResults);
+        searchResults.addAll(hotelResults);
 
     }
 
